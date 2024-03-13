89c260ed673df58ea13f7dbfe42881e4fa05e746
marcelaraujo1440 <162705599+marcelaraujo1440@users.noreply.github.com>
2024-03-13 19:00:19 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+print('Escolha uma operação:')
+print('1-Adiçao')
+print('2-Subtração')
+print('3-Multiplicação')
+print('4-Divisão')
+print('5-Sair')
+
+escolha=input('Escolha uma operação: ')
+n1=float(input('Digite o 1 número: '))
+n2=float(input('Digite o 2 número: '))
+
+if escolha=='1':
+    resultado=(n1+n2)
+    print(f'o resultado da soma é de {resultado} ')
+elif escolha=='2':
+    resultado=(n1-n2)
+    print(f'O resultado da divisão foi de {resultado}')
+elif escolha=='3':
+    resultado=(n1*n2)
+    print(f'O resultado da multiplicação foi de {resultado}')
+elif escolha=='4':
+        resultado=(n1/n2)   
+        print(f'O resultado da divisão foi de {resultado}')
+elif escolha=='5':
+    print('Tente com outra operação, você está fora da calculadora!')
+else:
+    print('Não foi possível encontrar o que você deseja!')
+
