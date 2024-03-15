ab3f3bed0c7875326e70aa3b762633e678130c16
marcelaraujo1440 <162705599+marcelaraujo1440@users.noreply.github.com>
2024-03-15 08:23:10 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -6,6 +6,10 @@ print('4-Divisão')
 print('5-Sair')
 
 escolha=input('Escolha uma operação: ')
+if escolha=='5':
+    print('OK!, Você está fora da calculadora!')
+
+    
 n1=float(input('Digite o 1 número: '))
 n2=float(input('Digite o 2 número: '))
 
@@ -21,8 +25,7 @@ elif escolha=='3':
 elif escolha=='4':
         resultado=(n1/n2)   
         print(f'O resultado da divisão foi de {resultado}')
-elif escolha=='5':
-    print('Tente com outra operação, você está fora da calculadora!')
+
 else:
     print('Não foi possível encontrar o que você deseja!')
 
